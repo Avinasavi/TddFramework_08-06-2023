f98a8131af36b162eca044e870a19ffa8df42ee9
suhil <suhil@LAPTOP-SO1NHVLU>
2023-06-08 21:11:57 +0530

    Third Commit..

--- DemoGitProject/src/sampleGitDay_01/DivNumbers.java
+++ DemoGitProject/src/sampleGitDay_01/DivNumbers.java
@@ -1,5 +1,8 @@
 package sampleGitDay_01;
 
-public class DivNumbers {
-
+public class DivNumbers {	public static void main(String[] args) {
+	 int a=1;int b=2;
+	int c=a/b;
+	System.out.println(c);
+}
 }
